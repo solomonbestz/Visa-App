0d5087c4fe9a67bc8abccff4de26a414c717d6c6
solomonbestz <kingsolodawarrior@gmail.com>
2024-04-29 15:29:45 +0100

    Updating the code base

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Hp 840\Documents\Codes\Visa Application\venv
+set VIRTUAL_ENV=C:\Users\Bestz\Documents\Codes\Projects\Visa-App\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
